78a8e455214a9f8a1b3ba8c0d4ea3d2af33e13a2
William Tong <williamtong105@gmail.com>
2022-01-21 19:24:55 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Finite-width BNN generalization
+
+This project numerically validates theoretical results on
+BNN generalization error.
+
+More (and better) documentation forthcoming.
---
+++ scratch.py
@@ -0,0 +1,16 @@
+"""BNN scratch work
+
+This notebook numerically validates generalization results for finite-width
+BNNs
+
+author: William Tong (wtong@g.harvard.edu)
+"""
+
+# <codecell>
+import numpy as np
+import matplotlib.pyplot as plt
+
+# <codecell>
+# TODO: sample data
+def build_sample():
+    pass
