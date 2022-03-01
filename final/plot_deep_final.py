Fix bug in theory calculation

final/plot_deep_final.py
@@ -317,6 +317,7 @@ else:
     np.save('deep_results_fig3.npy', results)
 
 # %%
+skip_idx = 2
 fig, axs_set = plt.subplots(4, 2, figsize=(8, 12))
 clip_const = 3
 
@@ -334,7 +335,7 @@ for i, ax in enumerate(axs_set[0]):
 
     fig.colorbar(ctr0, ax=ax)
 
-axs_set[0][0].set_title(r'$\sigma^2 = 1, \ell = 1$')
+axs_set[0][0].set_title(r'$\sigma^2 < 1, \ell = 1$')
 axs_set[0][1].set_title(r'$\sigma^2 > 1, \ell = 1$')
 
 for i, ax in enumerate(axs_set[1]):
@@ -351,7 +352,7 @@ for i, ax in enumerate(axs_set[1]):
 
     fig.colorbar(ctr0, ax=ax)
 
-axs_set[1][0].set_title(r'$\sigma^2 = 1, \ell > 1$')
+axs_set[1][0].set_title(r'$\sigma^2 < 1, \ell > 1$')
 axs_set[1][1].set_title(r'$\sigma^2 > 1, \ell > 1$')
 
 jitter = 0
@@ -377,7 +378,7 @@ for i, ax in enumerate(axs_set[2]):
         ax.grid(visible=True)
         ax.legend()
 
-axs_set[2][0].set_title(r'$\sigma^2 = 1, \ell = 1$')
+axs_set[2][0].set_title(r'$\sigma^2 < 1, \ell = 1$')
 axs_set[2][1].set_title(r'$\sigma^2 > 1, \ell = 1$')
 
 
@@ -406,7 +407,7 @@ for i, ax in enumerate(axs_set[3]):
         ax.grid(visible=True)
         ax.legend()
 
-axs_set[3][0].set_title(r'$\sigma^2 = 1$')
+axs_set[3][0].set_title(r'$\sigma^2 < 1$')
 axs_set[3][1].set_title(r'$\sigma^2 > 1$')
 
 # TODO: optimal calculation may not still work

final/very_deep_loss.py
@@ -19,7 +19,8 @@ def _theory_deep_full_p_small(a, sig, gs, eta=0):
     main_prod = sig ** 2 * (1 - a) * prod([((g - a) / g) * z + a * (1 - a + eta ** 2) / g for g in gs])
     poly = z ** (l + 1) - main_prod
 
-    def _keep_good(roots):
+    def _keep_good(roots, eta=0):
+        good_roots = []
         for r in roots:
             factors = []
             for g in gs:
@@ -27,9 +28,9 @@ def _theory_deep_full_p_small(a, sig, gs, eta=0):
                 factors.append(check > 0)
 
             if np.all(factors):
-                return r
-
-        return None
+                good_roots.append(r)
+        
+        return np.max(good_roots)
 
     try:
         # root = max(real_roots(poly)).evalf()
@@ -152,17 +153,32 @@ def compute_loss(p, d, ns, sig, eta, iters=5, inner_iters=100):
 
 # <codecell>
 # z = symbols('z')
-# l = 2
 # a = 0.5
-# gs = [0.1, 0.1]
+# gs = [1]
+# l = len(gs)
 
 # main_prod =(1 - a) * prod([((g - a) / g) * z + a * (1 - a) / g for g in gs])
 # poly = z ** (l + 1) - main_prod
 
-# def _is_good(r):
-#     return ((0.1 - 0.5)  * r + 0.5 * (0.5)) / (0.1 ** r) > 0
+# def _keep_good(roots, eta=0):
+#     good_roots = []
+#     for r in roots:
+#         factors = []
+#         for g in gs:
+#             check = ((g - a) * r + a * (1 - a + eta ** 2)) / g ** r
+#             factors.append(check > 0)
 
-# roots = [r.evalf() for r in real_roots(poly) if _is_good(r)]
+#         if np.all(factors):
+#             good_roots.append(r)
+    
+#     return np.max(good_roots)
 
-# print(roots)
+# roots = [r.evalf() for r in real_roots(poly)]
+# root = _keep_good(roots)
+
+# root_alt = np.max(roots)
+
+
+# print(root)
+# print(root_alt)
 # %%